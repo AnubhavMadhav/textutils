a7023e3b3babdc793f5b09b46aad5adb4c589b02
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-27 01:08:32 +0530

    Almost all bugs fixed

--- textutils/views.py
+++ textutils/views.py
@@ -28,14 +28,14 @@ def navigate(request):
 
 def analyze(request):
     # Get the Text
-    djtext = request.GET.get('text','default')                # Statement to get the values of the textarea and if not found then take default value
+    djtext = request.POST.get('text','default')                # Statement to get the values of the textarea and if not found then take default value
     # Check checkbox value
-    removepunc = request.GET.get('removepunc','off')            # Here, default will be 'off'
-    fullcaps = request.GET.get('fullcaps','off')            # Here, default will be 'off'
-    newlineremover = request.GET.get('newlineremover','off')            # Here, default will be 'off'
-    spaceremover = request.GET.get('spaceremover','off')            # Here, default will be 'off'
-    extraspaceremover = request.GET.get('extraspaceremover','off')            # Here, default will be 'off'
-    charcount = request.GET.get('charcount','off')            # Here, default will be 'off'
+    removepunc = request.POST.get('removepunc','off')            # Here, default will be 'off'
+    fullcaps = request.POST.get('fullcaps','off')            # Here, default will be 'off'
+    newlineremover = request.POST.get('newlineremover','off')            # Here, default will be 'off'
+    spaceremover = request.POST.get('spaceremover','off')            # Here, default will be 'off'
+    extraspaceremover = request.POST.get('extraspaceremover','off')            # Here, default will be 'off'
+    charcount = request.POST.get('charcount','off')            # Here, default will be 'off'
     # print(removepunc)
     # print(djtext)
     # a = '''<h1>Remove Punctuations</h1>
@@ -50,47 +50,58 @@ def analyze(request):
         for char in djtext:
             if char not in punctuations:
                 analyzed = analyzed + char
+        djtext = analyzed
         params = {'purpose':'Removed Punctuations','analyzed_text':analyzed}
         # Analyze the Text
-        return render(request, 'analyze.html', params)
-    elif fullcaps == "on":
+        # return render(request, 'analyze.html', params)
+    if fullcaps == "on":
         analyzed = ""
         for char in djtext:
             analyzed = analyzed + char.upper()
+        djtext = analyzed
         params = {'purpose': 'Capitalized Text', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-    elif newlineremover == "on":
+        # return render(request, 'analyze.html', params)
+    if newlineremover == "on":
         analyzed = ""
         for char in djtext:
-            if char!= '\n':
+            if char != '\n' and char != '\r':           # we need to check for both '\n' and '\r' for newline
                 analyzed = analyzed + char
         params = {'purpose': 'Removed New Line', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-    elif spaceremover == "on":
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+    if spaceremover == "on":
         analyzed = ""
         for char in djtext:
             if char != ' ':
                 analyzed = analyzed + char
         params = {'purpose': 'Removed Space', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-    elif charcount == "on":
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+    if charcount == "on":
         analyzed = ""
         c = 0
         for char in djtext:
             c += 1
         params = {'purpose': 'Count total no. of characters', 'analyzed_text': 'Total no.of characters = ' + str(c)}
-        return render(request, 'analyze.html', params)
-    elif extraspaceremover == "on":
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+    if extraspaceremover == "on":
         analyzed = ""
-        for index,char in enumerate(djtext):
+        for index, char in enumerate(djtext):
             if djtext[index] == " " and djtext[index + 1] == " ":
                 pass
             else:
                 analyzed = analyzed + char
+
         params = {'purpose': 'Extra Spaces Removed', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-    else:
-        return HttpResponse("Error!!")
+        # djtext = analyzed
+        # return render(request, 'analyze.html', params)
+
+    if removepunc != "on" and fullcaps != "on" and newlineremover != "on" and spaceremover != "on" and charcount != "on" and extraspaceremover != "on":
+        return HttpResponse("Error!! Select something and then try again.")
+
+
+    return render(request, 'analyze.html', params)
 
 # def capitalizefirst(request):
 #     b = '''<h1>Capitalize First</h1>
